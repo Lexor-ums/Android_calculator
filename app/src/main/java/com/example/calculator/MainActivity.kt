9383bf1fd50ae5c3c03536705b772fdf3139f271
Lexor-ums <su.lexxor@gmail.com>
2019-07-08 14:34:10 +0400

    make single fragment

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,48 +3,35 @@ package com.example.calculator
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.text.method.ScrollingMovementMethod
-import android.util.Log
+
+import android.view.Menu
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
-import com.example.calculator.UI.InputFragment
-import com.example.calculator.UI.ResultFragment
-import com.example.calculator.operations.Operator
-import com.example.calculator.operations.Solver
+
+import com.example.calculator.UI.CalcFragment
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.input_fragment_layout.*
-import java.lang.Exception
 
-class MainActivity : AppCompatActivity(), InputFragment.OnOperationComplited {
+class MainActivity : AppCompatActivity(), CalcFragment.OnOperationComplited {
 
-    private lateinit var inputFrag: InputFragment
-    private lateinit var resultFrag: ResultFragment
+    private var inputFrag: CalcFragment = CalcFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        inputFrag = fragment_input as InputFragment
-        resultFrag = fragment_result as ResultFragment
+        supportFragmentManager.beginTransaction().add(fragmentView.id, inputFrag).commit()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onMenuOpened(featureId: Int, menu: Menu?): Boolean {
+        println("menu opened")
+        return true
+    }
     override fun onAttachFragment(fragment: Fragment?) {
-        if (fragment is InputFragment)
+        if (fragment is CalcFragment)
             fragment.setInputFragmentCallback(this)
     }
-
-    override fun showOperationResult(value: Number, result: Number, operationType: String) {
-        resultFrag.showOperationResult(value, result, operationType)
-    }
-
-    override fun calculationsComplited(value: Number) {
-        resultFrag.calculationsComplited(value)
-    }
-
-    override fun calculationsStarted(value: Number, operationType: String) {
-        resultFrag.calculationsStarted(value, operationType)
-    }
-
     /**
     //     * обработка события вводы цифр
     //     */
@@ -62,22 +49,22 @@ class MainActivity : AppCompatActivity(), InputFragment.OnOperationComplited {
     /**
      * обработка события удаления введённого символа
      */
-    fun onEraseClicked(@Suppress("UNUSED_PARAMETER") view: View) {
-        inputFrag.onEraseClicked()
+    fun onEraseClicked(view: View) {
+        inputFrag.onEraseClicked(view)
     }
 
     /**
      * обработка события сброса данных
      */
-    fun onClear(@Suppress("UNUSED_PARAMETER") view: View) {
-        inputFrag.onClear()
-        resultFrag.onClear()
+    fun onClear( view: View) {
+        inputFrag.onClear(view)
     }
 
     /**
      * обработка переключения на ввод чисел с плавающей точкой
      */
-    fun onIntDoubleSwitch(@Suppress("UNUSED_PARAMETER") view: View) {
-        inputFrag.onIntDoubleSwitch()
+    fun onIntDoubleSwitch(view: View) {
+        inputFrag.onIntDoubleSwitch(view)
     }
 }
+
