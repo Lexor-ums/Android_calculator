8dd5eb6d64cf9459da8ef733c6ae1d5b764de129
Lexor-ums <su.lexxor@gmail.com>
2019-07-18 15:54:59 +0400

    inject retrofit

---
+++ app/src/main/java/com/example/calculator/ui/financialfragment/financialmainfragment/FinancialMainFragmentViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.calculator.ui.financialfragment.financialmainfragment
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class FinancialFragmentViewModel @Inject constructor(): ViewModel(){
+
+}
